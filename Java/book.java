e1106730459a59a76a6b6ea3612058053f33cea8
Hasan2980 <hasansuhail29@icloud.com>
2025-10-11 20:53:10 +0100

    Create book.java

---
+++ Java/book.java
@@ -0,0 +1,10 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+public class Book extends LibraryItem {
+    public Book(String title, String author, String publicationDate, String category) throws InvalidLibraryItemException {
+        super(title, author, publicationDate, category);
+    }
+}
